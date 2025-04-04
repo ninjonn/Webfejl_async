382cbbf7c15dc6ba1e39be81504e94882fde4ed2
Szeplaki Dominik <szeplaki.dominik@BOLYAI.LOCAL>
2025-04-04 12:25:06 +0200

    minden

--- 1_promise_async/main.js
+++ 1_promise_async/main.js
@@ -21,3 +21,57 @@ buyApple1(5).then((value) => {
 }).catch((error) => {
     console.log(error)
 })
+
+//--------------------------------------------------------------------------------
+
+const buyApple2 = (appleNumber) => {
+    return new Promise((resolve,reject) => {
+        if(appleNumber < 5){
+            resolve('Vannak almák:' + appleNumber)
+        }
+        else{
+            reject('Nincsenek elég almák: ' + appleNumber)
+        }
+    })
+}
+
+buyApple2(3).then((value) => {
+    console.log(value)
+}).catch((error) => {
+    console.log(error)
+})
+
+buyApple2(5).then((value) => {
+    console.log(value)
+}).catch((error) => {
+    console.log(error)
+})
+
+setTimeout(() => {
+    
+}, 0);
+
+const buyApple3 = (appleNumber) => {
+    return new Promise((resolve,reject) => {
+        if(appleNumber < 5){
+            setTimeout(() =>{
+                resolve('Vannak almák 2: ' + appleNumber)
+            },2000)
+        }
+        else{
+            setTimeout(() =>{
+                reject('Nincsenek elég almák 2: ' + appleNumber)
+            },3000)        
+        }
+    })
+}
+
+buyApple3(4).then((value) => {
+    console.log(value)
+}).catch((error) => {
+    console.log(error)
+})
+
+const res2 = buyApple3(5)
+
+console.log(res2)
